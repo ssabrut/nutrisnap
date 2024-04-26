9f84f05649ddc249d502cc8be2d10313d71df680
ssabrut <michael.gunawan2002@gmail.com>
2024-04-26 21:52:33 +0700

    Initial Commit

---
+++ NutriSnap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutriSnap
+//
+//  Created by Michael Eko on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NutriSnap/NutriSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriSnapApp.swift
+//  NutriSnap
+//
+//  Created by Michael Eko on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NutriSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
